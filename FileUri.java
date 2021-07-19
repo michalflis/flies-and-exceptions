ee91814cb5ed96ef04ea63853b5ea2b2ff519ec3
michalflis <michalt.flis@gmail.com>
2021-07-19 18:00:38 +0200

    Created class to print reversed text

---
+++ FileUri.java
@@ -0,0 +1,13 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class FileUri {
+
+    public URI getFileUri (String resourceName) throws URISyntaxException {
+
+        URL fileUrl = getClass().getResource(resourceName);
+        return fileUrl.toURI();
+    }
+
+}
